e906d96179c77312b3a24ae5761aa5aafb0cfa50
Lorna <lornnaa@live.com>
2020-03-10 04:00:11 +0000

    update

--- MyWebsite/ClientApp/src/modules/contact/contact.component.ts
+++ MyWebsite/ClientApp/src/modules/contact/contact.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-contact',
     templateUrl: './contact.component.html'
 })
 export class ContactComponent implements OnInit {
-    constructor(private fb: FormBuilder) { }
+    constructor(private fb: FormBuilder,
+        private httpClient: HttpClient) { }
 
     ngOnInit() {
     }
-    
+
     contactForm = this.fb.group({
         name: ['', Validators.required],
         email: ['', Validators.required],
@@ -18,6 +20,11 @@ export class ContactComponent implements OnInit {
     });
 
     onSubmit() {
-      alert(this.contactForm.value);
+        alert(this.contactForm.value);
+
+        this.httpClient.post('https://localhost:44363/api/email/', this.fb).subscribe(
+            (response) => console.log(response),
+            (error) => console.log(error)
+        ); 
     }
 }
