09865718ef5e4fd47535c17ad289a109d0871272
Lorna <lornnaa@live.com>
2020-03-30 22:48:19 +0100

    update

--- MyWebsite/ClientApp/src/modules/shared/navbar/navbar.component.ts
+++ MyWebsite/ClientApp/src/modules/shared/navbar/navbar.component.ts
@@ -1,29 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { faHome, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faDownload, faBars } from '@fortawesome/free-solid-svg-icons';
 import { faDev } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html'
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-    faHome = faHome;
-    faDev = faDev;
-    faDownload = faDownload;
+  faHome = faHome;
+  faDev = faDev;
+  faDownload = faDownload;
+  faBars = faBars;
 
-    constructor() { }
+  constructor() { }
 
-    ngOnInit() { }
+  ngOnInit() { }
 
-    onToggle() {
-        var toggle = document.getElementById("toggle");
-        if (toggle != null) {
-            toggle.classList.toggle('active');
-        }
-
-        var overlay = document.getElementById('overlay');
-        if (overlay != null) {
-            overlay.classList.toggle('open');
-        }
+  onToggle() {
+    var toggle = document.getElementById("toggle");
+    if (toggle != null) {
+      toggle.classList.toggle('active');
     }
+
+    var overlay = document.getElementById('overlay');
+    if (overlay != null) {
+      overlay.classList.toggle('open');
+    }
+  }
 }
