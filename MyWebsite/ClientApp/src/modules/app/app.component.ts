3b2eb3468c34ea021c72e4baa42c5109d97995a6
Lorna <lorna.watson@epiphany.co.uk>
2020-07-14 23:43:41 +0100

    update abstracts

--- MyWebsite/ClientApp/src/modules/app/app.component.ts
+++ MyWebsite/ClientApp/src/modules/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core'; 
 import { MetaDataService } from "../../core/meta-data.service";
+import { Title, Meta } from "@angular/platform-browser/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent extends MetaDataService implements OnInit {
+  constructor(titleService: Title,
+    metaService: Meta
+  ) {
+    super(titleService, metaService);
+  }
+
   ngOnInit() {
     this.addInitialTags();
   }
