54ade0294ab97dc485882ab4156ec5cdbef1dbec
Lorna <lorna.watson@epiphany.co.uk>
2020-07-14 23:46:41 +0100

    added new email link in footer

--- MyWebsite/ClientApp/src/modules/shared/footer/footer.component.ts
+++ MyWebsite/ClientApp/src/modules/shared/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { faLinkedinIn, faGithub, faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
 export class FooterComponent implements OnInit {
+  faEnvelope = faEnvelope;
   faLinkedinIn = faLinkedinIn;
   faGithub = faGithub;
   faFacebook = faFacebook;
