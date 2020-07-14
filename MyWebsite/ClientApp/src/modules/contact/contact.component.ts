cd477778f8163c25165de2281debc983d1f47347
Lorna <lorna.watson@epiphany.co.uk>
2020-07-14 21:11:49 +0100

    update content

--- MyWebsite/ClientApp/src/modules/contact/contact.component.ts
+++ MyWebsite/ClientApp/src/modules/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-//import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -9,31 +9,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ContactComponent implements OnInit {
   constructor(
-    //private fb: FormBuilder,
-    //private httpClient: HttpClient
+    private fb: FormBuilder,
+    private httpClient: HttpClient
   ) { }
 
   ngOnInit() {
   }
 
-  //faMapMarkedAlt = faMapMarkedAlt;
+  faMapMarkedAlt = faMapMarkedAlt;
 
-  //contactForm = this.fb.group({
-  //  name: ['', Validators.required],
-  //  email: ['', Validators.required],
-  //  message: ['', Validators.required],
-  //});
+  contactForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    message: ['', Validators.required],
+  });
 
-  //onSubmit() {
-  //  var model = {
-  //    subject: 'Contact Form Submission for lorna.dev',
-  //    from: 'noreply@lorna.dev',
-  //    body: 'Hi Lorna, \n\n' + this.contactForm.value
-  //  };
+  onSubmit() {
+    alert('todo');
 
-  //  this.httpClient.post('https://c80716a3-7580-4188-bcca-709ba03ab0b8.comms.lorna.dev/api/email/', model).subscribe(
-  //      (response) => console.log(response),
-  //      (error) => console.log(error)
-  //  ); 
-  //}
+    //var model = {
+    //  subject: 'Contact Form Submission from lorna.dev',
+    //  from: 'noreply@lorna.dev',
+    //  body: 'Hi Lorna, \n\n' + this.contactForm.value
+    //};
+
+    //this.httpClient.post('https://c80716a3-7580-4188-bcca-709ba03ab0b8.comms.lorna.dev/api/email/', model).subscribe(
+    //    (response) => console.log(response),
+    //    (error) => console.log(error)
+    //); 
+  }
 }
