ce947b867806ff5dcf9ecbd9a67266eacc1a5ecc
Lorna <lorna.watson@epiphany.co.uk>
2020-07-17 22:32:22 +0100

    update

--- MyWebsite/ClientApp/src/modules/home/home.component.ts
+++ MyWebsite/ClientApp/src/modules/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { MetaDataService } from "../../core/meta-data.service";
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent extends MetaDataService implements OnInit {
   faMapMarkerAlt = faMapMarkerAlt;
 
-  constructor() { }
+  constructor(titleService: Title,
+    metaService: Meta
+  ) {
+    super(titleService, metaService);
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.updateHomeTag();
+  }
 }
 
