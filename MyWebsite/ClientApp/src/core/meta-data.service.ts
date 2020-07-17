5d0addd27558d8c8bbb786551b1863fcabf35e1a
Lorna <lorna.watson@epiphany.co.uk>
2020-07-17 02:49:34 +0100

    update meta tags for linkedin

--- MyWebsite/ClientApp/src/core/meta-data.service.ts
+++ MyWebsite/ClientApp/src/core/meta-data.service.ts
@@ -11,7 +11,7 @@ export abstract class MetaDataService {
   }
 
   title = 'Lorna Watson | Software Developer';
-  description = 'Preferably working full-stack, Lorna is a software developer based in Leeds, UK with a passion for programming and data analytics.';
+  description = 'Lorna is a software developer based in Leeds, UK with a passion for programming, spreadsheets and data analytics.';
   image = 'https://firebasestorage.googleapis.com/v0/b/my-website-cbec6.appspot.com/o/laptop-corsair2.jpeg?alt=media&token=06a8227e-99ab-452b-9095-2eb69d431992';
   url = 'https://lorna.dev';
 
@@ -28,12 +28,15 @@ export abstract class MetaDataService {
     this.metaService.updateTag({ name: 'og:title', content: pageTitle });
     this.metaService.updateTag({ name: 'og:url', content: this.url + '/' + partUrl });
     this.metaService.updateTag({ name: 'twitter:title', content: pageTitle });
+     
+    this.metaService.updateTag({ property: 'og:title', content: pageTitle });
+    this.metaService.updateTag({ property: 'og:url', content: this.url + '/' + partUrl });
   }
 
   addInitialTags() {
     this.titleService.setTitle(this.title);
     this.metaService.addTags([
-      { name: 'keywords', content: 'Lorna Watson, lorna.dev, lornasw93, programmer, software developer, full stack developer, full stack, developer, dev,  portfolio' },
+      { name: 'keywords', content: 'Lorna Watson, lorna.dev, lornasw93, programmer, software developer, full stack developer, full stack, developer, dev, portfolio' },
       { name: 'description', content: this.description },
       { name: 'robots', content: 'index, follow' },
       { name: 'author', content: 'Lorna Watson' },
@@ -44,6 +47,12 @@ export abstract class MetaDataService {
       { name: 'og:description', content: this.description },
       { name: 'og:image', content: this.image },
       { name: 'og:type', content: 'website' },
+
+      // LinkedIn
+      { property: 'og:title', content: this.title },
+      { property: 'og:url', content: this.url },
+      { property: 'og:description', content: this.description },
+      { property: 'og:image', content: this.image },
 
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:site', content: '@lornasw93' },
