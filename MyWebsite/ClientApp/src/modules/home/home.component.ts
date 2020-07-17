cf590405a0243420086fb816ac4e6af68fe14db1
Lorna <lorna.watson@epiphany.co.uk>
2020-07-17 01:12:25 +0100

    update

--- MyWebsite/ClientApp/src/modules/home/home.component.ts
+++ MyWebsite/ClientApp/src/modules/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  faMapMarkerAlt = faMapMarkerAlt;
+
   constructor() { }
 
   ngOnInit() { }
 }
+
